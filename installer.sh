7ad5e4c36c0c56cdf3459eaaf231e535502c31ff
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-10-24 03:52:39 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,10 +3,13 @@
 # SCRIPT : DOWNLOAD AND INSTALL plugin-mix 
 # ###########################################
 #
-# Command: wget wget https://raw.githubusercontent.com/tarekzoka/plugin-mix/main/nstaller.sh -qO - | /bin/sh
-wait
+# Command: wget wget https://raw.githubusercontent.com/tarekzoka/
+plugin-mix/main/installer.sh -qO - | /bin/sh
+#
+# ###################
 opkg install --force-overwrite  https://raw.githubusercontent.com/tarekzoka/plugin-mix/main/enigma2-plugin-extensions-3plugin_1.5_all.ipk
 wait
 sleep 2;
 exit 0
+
 
