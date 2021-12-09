35323184349516a539adf591b391a1c248c92e36
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-12-10 01:19:27 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,27 +6,17 @@
 # Command: wget https://raw.githubusercontent.com/tarekzoka/plugin-mix/main/installer.sh -qO - | /bin/sh
 #
 # ###########################################
-  
 #!/bin/sh
 #
 
-wget -O /var/volatile/tmp/3plugin_1.5_all.ipk "https://github.com/tarekzoka/plugin-mix/raw/main/enigma2-plugin-extensions-3plugin_1.5_all.ipk"
+wget -O /tmp/oscam_11.696-emu-r798_all.ipk "https://github.com/tarekzoka/Oscam_Ncam/raw/main/enigma2-plugin-softcams-oscam_11.696-emu-r798_all.ipk"
+
 wait
-opkg install --force-overwrite /tmp/*.ipk
+#!/bin/sh
+opkg install /tmp/*.ipk
 wait
-rm -r /var/volatile/tmp/enigma2-plugin-extensions-3plugin_1.5_all.ipk
-wait
+opkg install --force-overwrite /tmp/*.ipk wait
 sleep 2;
-exit 0
-
-fi
-
-exit 0
-
-
-
-
-exit 0
 
 
 
